e045fd68e90aa5e23e5a1753368b684dacfa2c39
Alexandre Doussot <doussot.alexandre@gmail.com>
2017-02-16 15:40:15 +0000

    Add heuristical player

--- battleships/src/game.py
+++ battleships/src/game.py
@@ -12,9 +12,6 @@ class Player:
 
 
 class RandomPlayer(Player):
-    def __init__(self):
-        Player.__init__(self)
-
     def play(self):
         shots = 0
         stack = [to_dual(x) for x in range(100)]
@@ -23,7 +20,32 @@ class RandomPlayer(Player):
             self.target.shoot(stack.pop())
             shots += 1
         print(f"{shots} shots!")
+        return shots
 
+class HeuristicalPlayer(Player):
+    def play(self):
+        shots = 0
+        stack = [to_dual(x) for x in range(100)]
+        shuffle(stack)
+        while self.target.any():
+            pos = stack.pop()
+            if self.target.shoot(pos):
+                shots += self.target_play(pos)
+        print(f"{shots} shots!")
+        return shots
+
+    def target_play(self, pos):
+        stack = []
+        add_to_stack(pos, stack)
+        shots = 0
+        while self.target.any() and stack:
+            x, y = stack.pop()
+            if x not in range(10) or y not in range(10):
+                continue
+            if self.target.shoot((x, y)):
+                shots += self.target_play((x, y))
+            shots += 1
+        return shots
 
 if __name__ == '__main__':
-    RandomPlayer().play()
+    HeuristicalPlayer().play()
--- battleships/src/helpers.py
+++ battleships/src/helpers.py
@@ -1,2 +1,10 @@
 def to_dual(i):
     return (int(i / 10), i % 10)
+
+def add_to_stack(pos, stack):
+    x, y = pos
+    stack.append((x + 1, y))
+    stack.append((x - 1, y))
+    stack.append((x, y + 1))
+    stack.append((x, y - 1))
+
